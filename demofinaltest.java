08454bdeb462fd55a5a423dc55032a786cc0f294
Maryamkusman <93306676+Maryamkusman@users.noreply.github.com>
2022-02-22 13:16:39 -0800

    new

--- demofinaltest.java
+++ demofinaltest.java
@@ -1,4 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
 public class demofinaltest {
+    @Test
+    public void addition() {
+        assertEquals(2, demo1.subtract(3, 3));
+    }
     
     
 }
