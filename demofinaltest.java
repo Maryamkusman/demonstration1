e24622d7e741b3ff6f855b423c3556b01f6a2a1d
Maryamkusman <93306676+Maryamkusman@users.noreply.github.com>
2022-02-22 13:19:07 -0800

    fixed test

--- demofinaltest.java
+++ demofinaltest.java
@@ -3,7 +3,7 @@ import org.junit.*;
 public class demofinaltest {
     @Test
     public void addition() {
-        assertEquals(2, demo1.subtract(3, 3));
+        assertEquals(2, demo1.subtract(3, 1));
     }
     
     
